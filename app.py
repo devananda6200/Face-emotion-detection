04ffc8fb5592ba626026c676ddaa980c0a061fae
devananda6200 <devananda6200@gmail.com>
2026-06-19 20:57:21 +0530

    Fix Streamlit deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import tensorflow as tf
+import keras
 import numpy as np
 import cv2
 
@@ -36,7 +37,7 @@ st.markdown("""
 
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model(
+    return keras.models.load_model(
         "deep_cnn_model.keras",
         compile=False
     )
